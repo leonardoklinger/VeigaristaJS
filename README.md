5cb6941590bdecef9e36a3f3511b7d6ebed807a4
Leonardo Klinger <gamefivebr@gmail.com>
2022-07-15 19:11:13 -0300

    Mensagem de erros, callbacks. Adicionados e validações.

--- README.md
+++ README.md
@@ -5,3 +5,48 @@ Ops: Primeira vez programando em TypeScript, sua ajuda é sempre bem-vinda.
 
 It is a SourceOpen library, it aims to help in web scraping of data from the riot games API https://developer.riotgames.com/ (League of Legends only). 
 Oops: First time programming in TypeScript, your help is always welcome.
+
+## Como iniciar
+~~~javascript
+import { VeigaristaJS } from "./src"
+const veigar = new VeigaristaJS("Sua-Key-Vem-Aqui")
+~~~
+
+## Funções disponiveis
+~~~javascript
+veigar.procurarUserPorNome("Mateus verme", "br1", (resposta) =>{
+    console.log(resposta)
+})
+
+veigar.procurarUserPorContaId("qKQ8JPbLprrzwtzYpVGcQ4SoyUkO7BcrW2D6BTASfjxPUrpyPiVsE2eF", "br1", (resposta) => {
+    console.log(resposta)
+})
+
+veigar.procurarUserPorPuuId("tnqaSTx5y_ujguwF9znP1XLixeCZgw4xirIZH2cOoYVkYylYg4Z19FRIJROAL0Xi0fUc7BmHUBKTRg", "br1", (resposta) => {
+    console.log(resposta)
+})
+
+veigar.procurarUserPorId("BwmWNvYLcQFpRoU_OCyj8BrPqMlelhmM9uumCWFZu57lCks1", "br1", (resposta) => {
+    console.log(resposta)
+})
+
+veigar.maestriaUser("BwmWNvYLcQFpRoU_OCyj8BrPqMlelhmM9uumCWFZu57lCks", "br1", (resposta) => {
+    console.log(resposta)
+})
+
+veigar.maestriaUserCampeaoEspecifico("BwmWNvYLcQFpRoU_OCyj8BrPqMlelhmM9uumCWFZu57lCks", 45, "br1", (resposta) => {
+    console.log(resposta)
+})
+
+veigar.campeaoRotacao("br1", (resposta) => {
+    console.log(resposta)
+})
+
+veigar.topPosicaoCadaElo("br1", "soloDuo", "Desafiante", "I", 1, (resposta) => {
+    console.log(resposta)
+})
+
+veigar.statusLol("br1", (resposta) => {
+    console.log(resposta)
+})
+~~~
--- index.ts
+++ index.ts
@@ -2,38 +2,38 @@ import { VeigaristaJS } from "./src"
 
 const veigar = new VeigaristaJS("Sua-Key-Vem-Aqui")
 
-/* veigar.procurarUserPorNome("Mateus verme", "br1", (resposta) =>{
+veigar.procurarUserPorNome("Mateus verme", "br1", (resposta) =>{
     console.log(resposta)
-}) */
+})
 
-/* veigar.procurarUserPorContaId("qKQ8JPbLprrzwtzYpVGcQ4SoyUkO7BcrW2D6BTASfjxPUrpyPiVsE2eF", "br1", (resposta) => {
+veigar.procurarUserPorContaId("qKQ8JPbLprrzwtzYpVGcQ4SoyUkO7BcrW2D6BTASfjxPUrpyPiVsE2eF", "br1", (resposta) => {
     console.log(resposta)
-}) */
+})
 
-/* veigar.procurarUserPorPuuId("tnqaSTx5y_ujguwF9znP1XLixeCZgw4xirIZH2cOoYVkYylYg4Z19FRIJROAL0Xi0fUc7BmHUBKTRg", "br1", (resposta) => {
+veigar.procurarUserPorPuuId("tnqaSTx5y_ujguwF9znP1XLixeCZgw4xirIZH2cOoYVkYylYg4Z19FRIJROAL0Xi0fUc7BmHUBKTRg", "br1", (resposta) => {
     console.log(resposta)
-}) */
+})
 
-/* veigar.procurarUserPorId("BwmWNvYLcQFpRoU_OCyj8BrPqMlelhmM9uumCWFZu57lCks1", "br1", (resposta) => {
+veigar.procurarUserPorId("BwmWNvYLcQFpRoU_OCyj8BrPqMlelhmM9uumCWFZu57lCks1", "br1", (resposta) => {
     console.log(resposta)
-}) */
+})
 
-/* veigar.maestriaUser("BwmWNvYLcQFpRoU_OCyj8BrPqMlelhmM9uumCWFZu57lCks", "br1", (resposta) => {
+veigar.maestriaUser("BwmWNvYLcQFpRoU_OCyj8BrPqMlelhmM9uumCWFZu57lCks", "br1", (resposta) => {
     console.log(resposta)
-}) */
+})
 
-/* veigar.maestriaUserCampeaoEspecifico("BwmWNvYLcQFpRoU_OCyj8BrPqMlelhmM9uumCWFZu57lCks", 45, "br1", (resposta) => {
+veigar.maestriaUserCampeaoEspecifico("BwmWNvYLcQFpRoU_OCyj8BrPqMlelhmM9uumCWFZu57lCks", 45, "br1", (resposta) => {
     console.log(resposta)
-}) */
+})
 
-/* veigar.campeaoRotacao("br1", (resposta) => {
+veigar.campeaoRotacao("br1", (resposta) => {
     console.log(resposta)
-}) */
+})
 
-/* veigar.topPosicaoCadaElo("br1", "soloDuo", "Desafiante", "I", 1, (resposta) => {
+veigar.topPosicaoCadaElo("br1", "soloDuo", "Desafiante", "I", 1, (resposta) => {
     console.log(resposta)
-}) */
+})
 
-/* veigar.statusLol("br1", (resposta) => {
+veigar.statusLol("br1", (resposta) => {
     console.log(resposta)
-}) */
+})
